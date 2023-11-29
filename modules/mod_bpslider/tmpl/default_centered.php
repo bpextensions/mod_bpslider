482e5748d96a3a087ecfc6f0b3a0afc26b5ac36c
Artur Stępień <artur.stepien@bestproject.pl>
2023-11-29 18:35:11 +0100

    Fixing horizontal spacing

--- modules/mod_bpslider/tmpl/default_centered.php
+++ modules/mod_bpslider/tmpl/default_centered.php
@@ -25,29 +25,30 @@ defined('_JEXEC') or die;
 <?php if ($has_desc): ?>
     <div class="text-center swiper-bg-image d-flex align-items-center h-100"
 		<?php if (!empty($slide_image)): ?> style="background-image:url('<?php echo $slide_image ?>');"<?php endif ?>>
+        <div class="container-fluid">
+            <div class="row justify-content-center align-items-center">
 
-        <div class="row justify-content-center align-items-center w-100">
-
-            <!-- Desktop layout-->
-            <div class="col-12 col-lg-8 col-xl-6">
-                <div class="wrapper modbpslider-padding px-4 py-3">
-					<?php if (!empty($slide_title)): ?>
-                        <h3 class="title my-2"><?php echo SliderHelper::nl($slide_title, '<span class="d-none d-md-block"></span> ') ?></h3>
-                    <?php endif ?>
-					<?php if (!empty($slide_text)): ?>
-                        <div class="text my-2">
-							<?php echo $slide_text ?>
-                        </div>
-					<?php endif ?>
-					<?php if ($slide_button): ?>
-                        <a href="<?php echo SliderHelper::getButtonUrl($slide) ?>"
-                           class="btn btn-outline-light my-2">
-                            <?php echo $slide_button_title ?>
-                        </a>
-                    <?php endif ?>
+                <!-- Desktop layout-->
+                <div class="col-12 col-lg-8 col-xl-6">
+                    <div class="wrapper modbpslider-padding px-4 py-3">
+					    <?php if (!empty($slide_title)): ?>
+                            <h3 class="title my-2"><?php echo SliderHelper::nl($slide_title, '<span class="d-none d-md-block"></span> ') ?></h3>
+					    <?php endif ?>
+					    <?php if (!empty($slide_text)): ?>
+                            <div class="text my-2">
+							    <?php echo $slide_text ?>
+                            </div>
+					    <?php endif ?>
+					    <?php if ($slide_button): ?>
+                            <a href="<?php echo SliderHelper::getButtonUrl($slide) ?>"
+                               class="btn btn-outline-light my-2">
+							    <?php echo $slide_button_title ?>
+                            </a>
+					    <?php endif ?>
+                    </div>
                 </div>
-            </div>
 
+            </div>
         </div>
 
     </div>
--- modules/mod_bpslider/tmpl/default_right.php
+++ modules/mod_bpslider/tmpl/default_right.php
@@ -24,29 +24,30 @@ use BPExtensions\Module\BPSlider\Site\Helper\SliderHelper;
 ?>
 <?php if ($has_desc): ?>
     <div class="swiper-bg-image d-flex align-items-center h-100" <?php if (!empty($slide_image)): ?> style="background-image:url('<?php echo $slide_image ?>');"<?php endif ?>>
+        <div class="container-fluid">
+            <div class="row justify-content-end align-items-center">
 
-        <div class="row justify-content-end align-items-center">
-
-            <!-- Desktop layout-->
-            <div class="col-12 col-lg-6 col-xl-4">
-                <div class="wrapper modbpslider-padding px-4 py-3">
-					<?php if (!empty($slide_title)): ?>
-                        <h3 class="title my-2"><?php echo SliderHelper::nl($slide_title, '<span class="d-none d-md-block"></span> ') ?></h3>
-                    <?php endif ?>
-					<?php if (!empty($slide_text)): ?>
-                        <div class="text my-2">
-							<?php echo $slide_text ?>
-                        </div>
-					<?php endif ?>
-					<?php if ($slide_button): ?>
-                        <a href="<?php echo SliderHelper::getButtonUrl($slide) ?>"
-                           class="btn btn-outline-light my-2">
-                            <?php echo $slide_button_title ?>
-                        </a>
-                    <?php endif ?>
+                <!-- Desktop layout-->
+                <div class="col-12 col-lg-6 col-xl-4">
+                    <div class="wrapper modbpslider-padding px-4 py-3">
+					    <?php if (!empty($slide_title)): ?>
+                            <h3 class="title my-2"><?php echo SliderHelper::nl($slide_title, '<span class="d-none d-md-block"></span> ') ?></h3>
+					    <?php endif ?>
+					    <?php if (!empty($slide_text)): ?>
+                            <div class="text my-2">
+							    <?php echo $slide_text ?>
+                            </div>
+					    <?php endif ?>
+					    <?php if ($slide_button): ?>
+                            <a href="<?php echo SliderHelper::getButtonUrl($slide) ?>"
+                               class="btn btn-outline-light my-2">
+							    <?php echo $slide_button_title ?>
+                            </a>
+					    <?php endif ?>
+                    </div>
                 </div>
-            </div>
 
+            </div>
         </div>
 
     </div>
